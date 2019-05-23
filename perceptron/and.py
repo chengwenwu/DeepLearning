c85d947285c48826a473c6038828d9340697d9eb
chengwenwu <2681595858@qq.com>
2019-05-23 19:55:19 +0800

    添加了相关文件

---
+++ perceptron/and.py
@@ -0,0 +1,21 @@
+#  This program will complete the logic compute And
+
+import numpy as np
+
+class Perception:
+    def __init__(self, filename, vec_dim):
+        self.w = 0
+        self.d = 0
+
+
+    def single_perception_train(self, x, y):
+        """
+        This function give a way to train the model
+        :param x: input vector include x1 and x2
+        :param y: output of model
+        :return:
+        """
+
+
+
+
